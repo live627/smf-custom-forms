472ae07be3ade549a7b2c64f9161f77505410d50
John Rayes <live627@gmail.com>
2015-03-12 18:25:05 -0700

    Forgot this file

---
+++ add_remove_hooks.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @package CustomForms
+ * @since 1.0
+ */
+if (file_exists(__DIR__ . '/SSI.php') && !defined('SMF')) {
+	$ssi = true;
+	require_once(__DIR__ . '/SSI.php');
+} elseif (!defined('SMF')) {
+	exit('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
+}
+
+require_once(__DIR__ . '/CustomForms/ModHelper/Psr4AutoloaderClass.php');
+// instantiate the loader
+$loader = new \ModHelper\Psr4AutoloaderClass;
+// register the autoloader
+$loader->register();
+// register the base directories for the namespace prefix
+$loader->addNamespace('ModHelper', __DIR__ . '/CustomForms/ModHelper');
+$loader->addNamespace('CustomForms', __DIR__ . '/CustomForms');
+
+(new \ModHelper\Hooks)->add('integrate_pre_include', '$sourcedir/CustomForms/ModHelper/Psr4AutoloaderClass.php')
+	->add('integrate_pre_include', '$sourcedir/CustomForms/Integration.php')
+	->add('integrate_load_theme', '\\CustomForms\\Integration::load_theme')
+	->add('integrate_actions', '\\CustomForms\\Integration::actions')
+	->add('integrate_menu_buttons', '\\CustomForms\\Integration::menu_buttons')
+	->add('integrate_admin_areas', '\\CustomForms\\Integration::admin_areas')
+	->execute(empty($context['uninstalling']));
+
+if (!empty($ssi)) {
+	echo 'Database installation complete!';
+}
